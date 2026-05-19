96860fe39acf70a174a92d3495704876293a868d
Paul C <polkeldev@gmail.com>
2026-05-19 10:36:15 -0400

    API for the speech.

--- main.py
+++ main.py
@@ -1,46 +1,27 @@
 from util.generate_voice import create_speech
-import sys
 import os
-import subprocess
-import time
 from dotenv import load_dotenv
+from util.speaker import Speaker
+from fastapi import FastAPI, status
+
+app = FastAPI()
+
+load_dotenv()
+
+speaker_mac = os.getenv("SPEAKER_MAC")
+
+if speaker_mac is None:
+    raise ValueError("Must define SPEAKER_MAC in .env")
+
+speaker = Speaker(speaker_mac)
 
 
-def main():
-    load_dotenv()
-
-    speech = sys.argv[1].strip()
-
-    speaker_mac = os.getenv("SPEAKER_MAC")
-
-    if speaker_mac is None:
-        raise ValueError("Must have SPEAKER_MAC defined in .env")
-
-    if speech == "":
-        raise ValueError("Must pass in a speech argument.")
-
-    voice_file = create_speech(speech)
-
-    commands: list[tuple[list[str], bool]] = [
-        (["pulseaudio", "--start"], False),
-        (["bluetoothctl", "power", "on"], False),
-        (["bluetoothctl", "connect", speaker_mac], True),
-        (["pactl", "set-sink-volume", "@DEFAULT_SINK@", "100%"], False),
-        (["paplay", voice_file], False),
-        (["bluetoothctl", "power", "off"], False),
-    ]
-
-    for command, timer_pause in commands:
-        result = subprocess.run(command, capture_output=True, text=True)
-        if result.returncode != 0:
-            print(f"Ran into an error with {' '.join(command)}")
-            print(result.stdout)
-            print(result.stderr)
-
-        if timer_pause:
-            time.sleep(1)
-
-    os.remove(voice_file)
+@app.get("/")
+async def hello_world() -> dict[str, str]:
+    return {"message": "Hello World"}
 
 
-main()
+@app.post("/speech", status_code=status.HTTP_204_NO_CONTENT)
+async def queue_speech(speech: str):
+    speaker.queue_speech(speech)
+    return None
